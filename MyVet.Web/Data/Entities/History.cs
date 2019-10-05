96884a1dc8dad84649d74826642fc96c5ad31aac
ibaraybar <ibaraybard@gmail.com>
2019-10-05 18:57:36 -0500

    Add user entities

---
+++ MyVet.Web/Data/Entities/History.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class History
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Description*")]
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Description { get; set; }
+
+        [Display(Name = "Date*")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+        public string Remarks { get; set; }
+
+        [Display(Name = "Date*")]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}")]
+        public DateTime DateLocal => Date.ToLocalTime();
+
+        public ServiceType ServiceType { get; set; }
+    }
+}
---
+++ MyVet.Web/Data/Entities/User.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class User : IdentityUser
+    {
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [MaxLength(30, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string Document { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Display(Name = "Fixed Phone")]
+        public string FixedPhone { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Display(Name = "Cell Phone")]
+        public string CellPhone { get; set; }
+
+        [MaxLength(100, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        public string Address { get; set; }
+
+        //public string FullName 
+        //{ 
+        //    get
+        //    {
+        //        return $"{FirstName} {LastName}";
+        //    }
+        //}
+
+        [Display(Name = "Owner")]
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+    }
+}

093e6fc6047c2b3e2e36130496264beac65db683
ibaraybar <ibaraybard@gmail.com>
2019-10-08 21:35:25 -0500

    Add complex API controller

---
+++ MyVet.Web/Controllers/API/PetTypesController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyVet.Web.Data;
+using MyVet.Web.Data.Entities;
+
+namespace MyVet.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetTypesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PetTypesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PetTypes
+        [HttpGet]
+        public IEnumerable<PetType> GetPetTypes()
+        {
+            return _context.PetTypes;
+        }
+
+        // GET: api/PetTypes/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPetType([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var petType = await _context.PetTypes.FindAsync(id);
+
+            if (petType == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(petType);
+        }
+
+    }
+}

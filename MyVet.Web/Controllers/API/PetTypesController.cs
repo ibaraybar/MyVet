262dc6e32f8082c6bd89dd02a52efb6d1cde03a3
ibaraybar <ibaraybard@gmail.com>
2019-10-08 21:58:44 -0500

    Add Token to API

--- MyVet.Web/Controllers/API/PetTypesController.cs
+++ MyVet.Web/Controllers/API/PetTypesController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MyVet.Web.Data;
 using MyVet.Web.Data.Entities;
@@ -8,6 +10,7 @@ namespace MyVet.Web.Controllers.API
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class PetTypesController : ControllerBase
     {
         private readonly DataContext _context;
--- MyVet.Web/Helpers/IUserHelper.cs
+++ MyVet.Web/Helpers/IUserHelper.cs
@@ -27,5 +27,7 @@ namespace MyVet.Web.Helpers
         Task<bool> DeleteUserAsync(string email);
 
         Task<IdentityResult> UpdateUserAsync(User user);
+
+        Task<SignInResult> ValidatePasswordAsync(User user, string password);
     }
 }
